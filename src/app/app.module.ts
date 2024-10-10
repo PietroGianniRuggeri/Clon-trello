8dce749c49b2959b52e0b1968a5445ffb68dd1d9
Pietrus <pruggeri@asap-consulting.netclear>
2024-10-10 18:06:38 -0300

    Add new library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    OverlayModule,
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: colors.green,
+        warning: colors.yellow,
+        primary: colors.blue,
+        danger: colors.red,
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
