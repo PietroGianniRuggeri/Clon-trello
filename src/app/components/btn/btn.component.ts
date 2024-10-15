753ef604b3c800910208d9991f415f3003ac8397
Pietrus <pruggeri@asap-consulting.netclear>
2024-10-15 15:56:42 -0300

    Btn custon

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -7,34 +7,58 @@ import { Component, Input, OnInit } from '@angular/core';
 export class BtnComponent implements OnInit {
 
   @Input() typeBtn:  'button' | 'reset' |'submit' = 'button';
-  @Input() color: string = 'primary';
+  @Input() color: 'success' | 'warning' | 'danger' | 'primary' | 'light' | 'sky' = 'primary';
+
+  mapColors = {
+    success: {
+      'bg-success-700': true,
+      'hover:bg-success-800': true,
+      'focus:ring-success-300': true,
+      'text-white': true
+    },
+    warning: {
+      'bg-warning-700': true,
+      'hover:bg-warning-800': true,
+      'focus:ring-warning-300': true,
+      'text-white': true    
+    },
+    danger: {
+      'bg-danger-700': true,
+      'hover:bg-danger-800': true,
+      'focus:ring-danger-300': true,
+      'text-white': true 
+    },
+    primary: {
+      'bg-primary-700': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true
+    },
+    sky: {
+      'bg-primary-700': true,
+      'hover:bg-primary-800': true,
+      'focus:ring-primary-300': true
+    },
+    light: {
+      'bg-gray-700': true,
+      'hover:bg-gray-800': true,
+      'focus:ring-gray-300': true,
+      'text-gray-800': true
+    }
+
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  get colors() {
-    /* return `bg-${this.color}-700 hover:bg-${this.color}-800 focus:ring-${this.color}-300`; */
-    return{
-      'text-white': this.color === 'success' || this.color === 'warning' || this.color === 'danger' || this.color === 'primary',
-      'text-gray-800': this.color === 'gray-light',
-      'bg-success-700': this.color === 'success',
-      'hover:bg-success-800': this.color === 'success',
-      'focus:ring-success-300': this.color === 'success',
-      'bg-warning-700': this.color === 'warning',
-      'hover:bg-warning-800': this.color === 'warning',
-      'focus:ring-warning-300': this.color === 'warning',
-      'bg-primary-700': this.color === 'primary',
-      'hover:bg-primary-800': this.color === 'primary',
-      'focus:ring-primary-300': this.color === 'primary',
-      'bg-danger-700': this.color === 'danger',
-      'hover:bg-danger-800': this.color === 'danger',
-      'focus:ring-danger-300': this.color === 'danger',
-      'bg-gray-200': this.color === 'gray-light',
-      'hover:bg-gray-500': this.color === 'gray-light',
-      'focus:ring-danger-50': this.color === 'gray-light',
-    }
+  get colors() {    
+     const colors = this.mapColors[this.color];
+     if (colors) {
+      return colors;
+     }
+     return {};
+    
   }
 
 }
